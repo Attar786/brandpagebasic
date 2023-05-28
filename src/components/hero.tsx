3d3dbf90040769be1fb6ef0a81a9e206ac3cf5a8
Atta_ur_rehman <attaurr786@outlook.com>
2023-05-28 18:36:27 +0500

    completed

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,17 +1,33 @@
 const HeroSection = () => 
 {
     return(
-<main>
+<main className="hero">
     <div className="hero-content">
+    
 <h1>YOUR FEET 
 DESERVE
 THE BEST</h1>
 <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
-    </div>
-    <div className="buttons">
+   
+    <div className="hero-btn">
         <button>ShopNow</button>
-        <button>catagories</button>
+        <button className="second-btn">catagories</button>
     </div>
+    <div className="shopping">
+
+    <p>Also Available on</p>
+  
+    <div className="brand-icons">
+<img src="/src/images/amazon.png"/>
+<img src="/src/images/flipkart.png"/>
+</div>
+</div>
+</div>
+    <div>
+            <img src="/src/images/shoe_image.png"/>
+    </div>
+    
+
 </main>
     );
 };
